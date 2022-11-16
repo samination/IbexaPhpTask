42a596004c87acc87cc7a43bf93ee44434d38568
samination <43864365+samination@users.noreply.github.com>
2022-11-16 15:54:55 +0100

    Implemented Load Balancing Task

--- Classes/Host.php
+++ Classes/Host.php
@@ -1,6 +1,32 @@
 <?php
 
+use http\Client\Request;
+
 class Host
 {
+    private float $Load;
+
+    /**
+     * @param float $Load
+     */
+    public function __construct(float $Load)
+    {
+        $this->Load = $Load;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLoad(): float
+    {
+        return $this->Load;
+    }
+
+
+    public function HandleRequest(Request $request):void
+{
+        //Handle Request Code
+}
+
 
 }
--- Classes/LoadBalancer.php
+++ Classes/LoadBalancer.php
@@ -1,6 +1,51 @@
 <?php
 
+
+use http\Client\Request;
+
 class LoadBalancer
 {
+    /**
+     * @var Host[] The hosts
+     */
+    private array $hosts;
+    private int $variant;
+    // Variant defined as int if variant value is 0 then the request executes the first algorithm if variant value is 1 then it executes the second algorithm
+    public function __construct(int $variant,Host ...$hosts)
+    {
+        $this->hosts = $hosts;
+        $this->variant=$variant;
+    }
+    public function HandleRequest(Request $request)
+    {
+        $minLoadHost =new Host(-1);
+
+        foreach ($this->hosts as $host )
+        {
+            if($this->variant==0)
+            {
+                $host->HandleRequest($request);
+            }
+            elseif ($this->variant==1)
+            {
+                if($host->getLoad()<0.75) {
+                    //Retruns the first host that has a load inferior than 0.75
+                    return $host;
+                }
+                else {
+                        if($host->getLoad()<$minLoadHost->getLoad())
+                        {
+                            $minLoadHost=$host;
+                        }
+                }
+            }
+
+
+        }
+        //Returns the the host with the least load in case all loads are above 0.75
+        // if the load is -1 then the first variant was executed
+        return $minLoadHost;
+    }
+
 
 }
